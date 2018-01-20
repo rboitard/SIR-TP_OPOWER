686f9c6bf037bae3fc71a19c41e63972323fac2d
romain <rboitard@gmail.com>
2018-01-20 14:58:26 +0100

    reupload JpaTest

---
+++ src/main/java/jpa/JpaTest.java
@@ -0,0 +1,51 @@
+package jpa;
+
+
+
+
+import domain.Person;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.Iterator;
+import java.util.List;
+
+public class JpaTest {
+
+    public EntityManager manager;
+
+    public JpaTest( EntityManager manager) {
+        this.manager = manager;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("mysql");
+        EntityManager manager = factory.createEntityManager();
+        EntityTransaction tx = manager.getTransaction();
+        tx.begin();
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        tx.commit();
+
+        manager.close();
+        factory.close();
+        System.out.println("done ...");
+    }
+
+    public List<Person> listPerson() {
+        return  manager.createQuery("Select a From Person a", Person.class).getResultList();
+    }
+}
+
+
+
+
+
