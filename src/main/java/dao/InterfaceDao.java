ed62aaec297c9169d8a946b2849b74c0386b9447
romain <rboitard@gmail.com>
2018-01-25 11:10:16 +0100

    update person dao
    change id Long to it

---
+++ src/main/java/dao/InterfaceDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+public interface InterfaceDao {
+
+    public void create( Object obj);
+
+    public void update( Object obj);
+
+    public void delete( Object obj);
+
+    public void get();
+}
---
+++ src/main/java/dao/PersonDao.java
@@ -0,0 +1,61 @@
+package dao;
+
+import domain.Person;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.Iterator;
+
+public class PersonDao {
+
+
+    public Person create(Person p){
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("mysql");
+        EntityManager manager = factory.createEntityManager();
+        EntityTransaction tx = manager.getTransaction();
+        tx.begin();
+        manager.persist(p);
+        tx.commit();
+        manager.close();
+        factory.close();
+        return p;
+    }
+
+    public Person read(int id) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("mysql");
+        EntityManager manager = factory.createEntityManager();
+        Person person = manager.find(Person.class, id);
+        manager.close();
+        factory.close();
+        return person;
+
+    }
+
+
+
+    public Person update(Person p) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("mysql");
+        EntityManager manager = factory.createEntityManager();
+        EntityTransaction tx = manager.getTransaction();
+        tx.begin();
+        manager.merge(p);
+        tx.commit();
+        manager.close();
+        factory.close();
+        return p;
+
+    }
+
+
+
+    public void delete(Person p) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("mysql");
+        EntityManager manager = factory.createEntityManager();
+        manager.remove(p);
+        manager.flush();
+        manager.close();
+        factory.close();
+    }
+}
